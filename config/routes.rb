7d05970c9bdec3f7707d7e8b506dabdc141f56cf
Fred Coque <fredcoque@yahoo.com>
2020-11-12 18:47:21 -0600

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+  resources :beaches do
+    resources :reviews, only: [:index, :new, :create]
+    get "traffic", to: "beaches#traffic"
+    get "weather", to: "beaches#weather"
+    get "real_time", to: "beaches#real_time"
+  end
 end
