17c0a269526ab961e8f027e634247a438dc6e4ea
Fred Coque <fredcoque@yahoo.com>
2020-11-14 08:24:43 -0600

    Associations and validations updated

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -4,5 +4,5 @@ class Beach < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :lat, presence: true
   validates :lng, presence: true
-  validates :overall_rating, :numericality=> true, :inclusion => {:in => 0..10, :message => "Value should be between 0 and 10"}
+  validates :overall_rating, numericality: true, inclusion: {in: 0..10, message: 'Value should be between 0 and 10' }
 end
