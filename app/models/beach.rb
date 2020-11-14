81d0e5eb485fffd811acdb3a0c7a802824c8c79e
Fred Coque <fredcoque@yahoo.com>
2020-11-14 11:05:42 -0600

    Added overview length validation

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -4,5 +4,6 @@ class Beach < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :lat, presence: true
   validates :lng, presence: true
-  validates :overall_rating, numericality: true, inclusion: {in: 0..10, message: 'Value should be between 0 and 10' }
+  validates :overall_rating, numericality: true, inclusion: { in: 0..10, message: 'Value should be between 0 and 10' }
+  validates :overview, length: { minimum: 10, maximum: 254 }
 end
