4aa2e2715f85a39890decf5a654bc6f716ce262f
Alice Ratton <aliceratton@gmail.com>
2020-11-26 20:36:00 -0200

    correcting beach.rb

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -6,8 +6,6 @@ class Beach < ApplicationRecord
   validates :lat, presence: true
   validates :lng, presence: true
   validates :overview, length: { minimum: 10, maximum: 254 }
-<<<<<<< HEAD
-=======
+
   has_one_attached :photo
->>>>>>> 35f5b334c6fb1cbc167459f0fffb9f4e7f0b2c10
 end
