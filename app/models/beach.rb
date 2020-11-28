f32fe9cb240946a410c661d1e8698311448b1bbf
tobias ferreira <soutobias@yahoo.com.br>
2020-11-27 22:33:40 -0300

    add places

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,6 +1,9 @@
 class Beach < ApplicationRecord
   has_many :real_time_values, dependent: :destroy
   has_many :weather_forecast_values, dependent: :destroy
+  has_many :wave_forecast_values, dependent: :destroy
+  has_many :water_forecast_values, dependent: :destroy
+
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :lat, presence: true
